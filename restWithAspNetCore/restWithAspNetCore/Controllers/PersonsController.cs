f6081e4bf50c0ba5a270d69cc42a04b6455e4ad4
sergio.ramos <spramos@rd.com.br>
2020-06-16 00:20:59 -0300

    implementando os verbos principais

--- restWithAspNetCore/restWithAspNetCore/Controllers/PersonsController.cs
+++ restWithAspNetCore/restWithAspNetCore/Controllers/PersonsController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace restWithAspNetCore.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class PersonsController : Controller
     {
         // GET api/values
         [HttpGet]
---
+++ restWithAspNetCore/restWithAspNetCore/Model/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace restWithAspNetCore.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public long FirstName { get; set; }
+        public long LastName { get; set; }
+
+        public long Address { get; set; }
+        public long Gender { get; set; }
+    
+    }
+}
